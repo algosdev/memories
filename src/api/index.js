7209103c310527bcef02269b2e4b359f24adf335
Samandar <samandar149official@gmail.com>
2021-03-06 17:51:54 +0500

    updateD

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const url = 'http://localhost:5000/'
+const url = 'https://memories-algo.herokuapp.com/'
 // https://memories-algo.herokuapp.com/
 
 const API = axios.create({ baseURL: url })
